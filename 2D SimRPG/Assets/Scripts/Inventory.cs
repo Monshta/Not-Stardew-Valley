19317d1c4a47ffbd9681a54d6a9df6f09f3f3948
Mohammed Azad <Varsity.azad@gmail.com>
2016-08-30 23:08:22 -0700

    added inventory

---
+++ 2D SimRPG/Assets/Scripts/Inventory.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+    GameObject InventoryPanel;
+    GameObject SlotPanel;
+    public GameObject InventorySlot;
+    public GameObject InventoryItem;
+    ItemDatabase database;
+    int slotAmount;
+    public List<Item> Items = new List<Item>();
+    public List<GameObject> slots = new List<GameObject>();
+
+    void Start()
+    {
+        database = GetComponent<ItemDatabase>();
+        slotAmount = 18;
+        InventoryPanel = GameObject.Find("InventoryPanel");
+        SlotPanel = InventoryPanel.transform.FindChild("SlotPanel").gameObject;
+        for(int i = 0; i < 18; i++)
+        {
+            Items.Add(new Item());
+            slots.Add(Instantiate(InventorySlot));
+            slots[i].transform.SetParent(SlotPanel.transform);
+        }
+        AddItemByID(0);
+        AddItemByTitle("Wood");
+    }
+    public void AddItemByID(int id)
+    {
+        Item itemToAdd = database.SearchItemByID(id);
+        for(int i = 0; i < Items.Count; i++)
+        {
+            if(Items[i].ID == -1)
+            {
+                Items[i] = itemToAdd;
+                GameObject itemObject = Instantiate(InventoryItem);
+                itemObject.transform.SetParent(slots[i].transform);
+                itemObject.transform.position = Vector2.zero;
+                itemObject.GetComponent<Image>().sprite = itemToAdd.Sprite;
+                itemObject.name = itemToAdd.Title;
+                slots[i].name = "Slot" + itemToAdd.Title;
+                break;
+            }
+        }
+    }
+
+    public void AddItemByTitle(string title)
+    {
+        Item itemToAdd = database.SearchItemByTitle(title);
+        for (int i = 0; i < Items.Count; i++)
+        {
+            if (Items[i].ID == -1)
+            {
+                Items[i] = itemToAdd;
+                GameObject itemObject = Instantiate(InventoryItem);
+                itemObject.transform.SetParent(slots[i].transform);
+                itemObject.transform.position = Vector2.zero;
+                itemObject.GetComponent<Image>().sprite = itemToAdd.Sprite;
+                itemObject.name = itemToAdd.Title;
+                slots[i].name = "Slot" + itemToAdd.Title;
+                break;
+            }
+        }
+    }
+}
--- 2D SimRPG/Assets/Scripts/ItemDatabase.cs
+++ 2D SimRPG/Assets/Scripts/ItemDatabase.cs
@@ -18,8 +18,31 @@ public class ItemDatabase : MonoBehaviour {
     {
         for(int i= 0; i < ItemData.Count; i++)
         {
-            database.Add(new Item((int)ItemData[i]["id"], ItemData[i]["title"].ToString(), (int)ItemData[i]["value"]));
+            database.Add(new Item((int)ItemData[i]["id"], ItemData[i]["title"].ToString(), ItemData[i]["type"].ToString(),
+                (bool)ItemData[i]["stackable"],(bool)ItemData[i]["equipable"], (int)ItemData[i]["stats"]["attack"], 
+                (int)ItemData[i]["stats"]["defense"], (bool)ItemData[i]["consumable"], ItemData[i]["description"].ToString(), 
+                (int)ItemData[i]["buyValue"],(int)ItemData[i]["sellValue"],ItemData[i]["slug"].ToString()));
         }
+    }
+
+    public Item SearchItemByID(int id)
+    {
+        for(int i = 0; i < database.Count; i++)
+        {
+            if (database[i].ID == id)
+                return database[i];
+        }
+        return null;
+    }
+
+    public Item SearchItemByTitle(string title)
+    {
+        for (int i = 0; i < database.Count; i++)
+        {
+            if (database[i].Title == title)
+                return database[i];
+        }
+        return null;
     }
 }
 
@@ -27,12 +50,35 @@ public class Item
 {
     public int ID { get; set; }
     public string Title { get; set; }
-    public int Value { get; set; }
-
-    public Item(int id, string title, int value)
+    public string Type { get; set; }
+    public bool Stackable { get; set; }
+    public bool Equipable { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+    public bool Consumable { get; set; }
+    public string Description { get; set; }
+    public int BuyValue { get; set; }
+    public int SellValue { get; set; }
+    public string Slug { get; set; }
+    public Sprite Sprite { get; set; }
+    public Item(int id, string title, string type, bool stackable, bool equipable, int attack, int defense, bool consumable, string description, int buyValue, int sellValue, string slug )
     {
         this.ID = id;
         this.Title = title;
-        this.Value = value;
+        this.Type = type;
+        this.Stackable = stackable;
+        this.Equipable = equipable;
+        this.Attack = attack;
+        this.Defense = Defense;
+        this.Consumable = consumable;
+        this.Description = description;
+        this.BuyValue = buyValue;
+        this.SellValue = sellValue;
+        this.Slug = slug;
+        this.Sprite = Resources.Load<Sprite>("ItemSlugs/" + slug);
+    }
+    public Item()
+    {
+        this.ID = -1;
     }
 }
