015470ef65379fa7d0831f2a373a882c00ec24e6
Monshta <frgimena@gmail.com>
2016-09-04 09:37:28 -0700

    Added Depth

---
+++ 2D SimRPG/Assets/Scripts/Depth.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Depth : MonoBehaviour {
+
+    private float YPosition;
+    private float XPosition;
+
+	void Update () {
+        XPosition = transform.position.x;
+        YPosition = transform.position.y;
+        transform.position = new Vector3(XPosition, YPosition, YPosition);
+	}
+}
