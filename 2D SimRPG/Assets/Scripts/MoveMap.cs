61707305c329999ee04db325c74a7029942986e6
Mohammed Azad <Varsity.azad@gmail.com>
2016-09-27 12:30:29 -0700

    added opening doors

---
+++ 2D SimRPG/Assets/Scripts/MoveMap.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveMap : MonoBehaviour {
+    public GameObject SpawnPoint;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.name == "Character")
+        {
+            InvokeRepeating("IfRightClickOnDoor", 0, 0.01f);
+        }
+    }
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        CancelInvoke();
+    }
+    void IfRightClickOnDoor()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit2D hitTree = Physics2D.Raycast(new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y), Vector2.zero);
+            if (hitTree == true && hitTree.collider.gameObject == gameObject)
+            {
+                GameObject character = GameObject.Find("Character");
+                character.transform.position = SpawnPoint.transform.position;
+                GameObject.Find("Main Camera").GetComponent<CameraController>().xCon = new Vector3(64.2f, 65.7f, 0);
+                GameObject.Find("Main Camera").GetComponent<CameraController>().yCon = new Vector3(0.1f, 2f, 0);
+            }
+        }
+    }
+}
